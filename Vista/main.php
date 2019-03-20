d6b50bdda4376c5e7b6ca2c24560c99c8749f4ce
David Reitmaier <reitmaier18@gmail.com>
2019-03-20 03:45:42 -0400

    Arreglando el menú del sistema y vista de inicio del mismo

--- Vista/main.php
+++ Vista/main.php
@@ -13,38 +13,63 @@
     <link href="css/style.css" rel="stylesheet">
     
 </head>
-<body>
+<body id="body">
     <!--Este es el header-->
     <div class="container-fluid">
-        <header>
+        <!--header>
             <div class="header-logo">
                 <img src="img/logo2.png">   
             </div>
             <h1>Sistema de Control y Distribución de Expedientes</h1>
-        </header>
+        </header-->
         <!--Navbar -->
-        <nav class="mb-1 navbar navbar-expand-lg navbar-dark">            
+        <nav class="mb-1 navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">             
+            <div class="header-logo2"  class="navbar-brand">
+                <img src="img/logo.png" title="Jurisdicción Disciplinaria Judicial">   
+            </div>
+            
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333" aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
-                <ul class="navbar-nav mr-auto">
+                <ul class="navbar-nav nav-pills mr-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Inicio
-                            <span class="sr-only">(current)</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Expedientes</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Solicitudes</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Auditoria</a>
+                        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item dropdown">
-                     <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown </a>
+                     <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Expedientes </a>
+                        <div class="dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Solicitudes </a>
+                        <div class="dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Auditoria </a>
+                        <div class="dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Reportes </a>
+                        <div class="dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Estadisticas </a>
                         <div class="dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
                             <a class="dropdown-item" href="#">Action</a>
                             <a class="dropdown-item" href="#">Another action</a>
@@ -59,6 +84,7 @@
                         </a>
                         <div class="dropdown-menu dropdown-menu-right dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
                             <a class="dropdown-item" href="login2.php">Cerrar Sesión</a>
+                            <a class="dropdown-item" href="#">Opciones</a>
                         </div>
                     </li>
                 </ul>
@@ -66,11 +92,22 @@
         </nav>
         <!--/.Navbar -->
     </div>
-
     
+    <section class="my-5">
+        <div class="container">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8" id="inicio">
+                <h2 class="h2-responsive font-weight-bold text-center my-5">¡Bienvenido! <br><br> usuario <br><br> Sistema de Control y Distribución de Expedientes</h2>
+                <p class="text-center"></p>
+            </div>
+        </div>
+        </div>
+
+    </section>
     
 	<!-- SCRIPTS -->
-    
+    <script type="text/javascript" src="js/efectos.js"></script>
     <!-- JQuery -->
     <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
     <!-- Bootstrap tooltips -->
